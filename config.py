77169e5a04dac3e83796a0820392501fcb4ead2a
jlrosa <rozzas_@hotmail.com>
2015-12-19 11:01:26 +0000

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ CONFIG = {
     'google': {
         'class_': oauth2.Google,
         'id': 3,
-        'consumer_key': '1023452814056-s6c5f9mbco09cdjbipi9lh8p707g08jh.apps.googleusercontent.com',
-        'consumer_secret': '6yzy1n9w_RVSchWYHhC7p-xI',
+        'consumer_key': 'CONSUMER_KEY',
+        'consumer_secret': 'CONSUMER_SECRET',
         'scope': oauth2.Google.user_info_scope,
     },
 }
